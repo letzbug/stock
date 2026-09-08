4f8640d9eaa1f2d79e2d10111f3459022bab10d7
letzbug <129280428+letzbug@users.noreply.github.com>
2026-09-08 07:51:44 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
   Leave both values empty to run this app locally on the device.
 */
 window.UNIPOP_STOCK_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://eiashmxnthdbpnmqgxga.supabase.co",
+  supabaseAnonKey: "sb_publishable_tEWw7PsrV3KCssYplZgYOQ_bpNQ0Lic"
 };
